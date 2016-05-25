a621f3f394c9f67388e34a82de0259bfbdfa4fb6
mariana3f.dsaupoo@gmail.com <canetemendez>
2016-05-25 14:26:00 -0500

    subida0010

--- js/acciones.js
+++ js/acciones.js
@@ -7,14 +7,14 @@ $(document).ready(function(e) {
 		
 	audio = window.plugins.LowLatencyAudio;
 		
-audio.preloadFX('do', 'audio/C.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-audio.preloadFX('re', 'audio/D.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-audio.preloadFX('mi', 'audio/E.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-audio.preloadFX('fa', 'audio/F.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-audio.preloadFX('sol', 'audio/G.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-audio.preloadFX('la', 'audio/A.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-audio.preloadFX('si', 'audio/B.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-audio.preloadFX('do', 'audio/C.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+audio.preloadFX('do', 'audio/DO.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+audio.preloadFX('re', 'audio/FA.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+audio.preloadFX('mi', 'audio/LA.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+audio.preloadFX('fa', 'audio/MI.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+audio.preloadFX('sol', 'audio/RE.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+audio.preloadFX('la', 'audio/SI.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+audio.preloadFX('si', 'audio/SOL.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+audio.preloadFX('do', 'audio/DO.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
 
 
 $('.nota').bind('touchstart', function(){
